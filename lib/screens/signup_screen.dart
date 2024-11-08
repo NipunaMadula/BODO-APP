91c6538ba0db80a249ab94461a8608244a44012e
Nipuna Madula <nipunamadula2001@gmail.com>
2024-11-09 01:48:54 +0530

    Update BODO APP header

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -6,11 +6,34 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: const Center(
-        child: Text('Sign Up Screen'),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            const SizedBox(height: 60),
+          
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              width: double.infinity,
+              child: const Text(
+                'BODO APP',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.2, 
+                ),
+              ),
+            ),
+            
+            const Expanded(
+              child: Center(
+                child: Text('Sign Up Form'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
