a5f5196e1f46fb02496d84725793376ba6ecc443
Nipuna Madula <nipunamadula2001@gmail.com>
2024-11-08 23:50:35 +0530

    Add SplashScreen with 3-second delay navigation to SignUpScreen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'signup_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+   
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SignUpScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.blue, 
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/img1.jpg', // Make sure to add this image to pubspec.yaml
+            width: 195.57,
+            height: 46.66,
+          ),
+        ),
+      ),
+    );
+  }
+}
