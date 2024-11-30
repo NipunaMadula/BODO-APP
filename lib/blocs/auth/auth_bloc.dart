47551cd82e66755dd9eee55bd0fb01bb5a71320c
Nipuna Madula <nipunamadula2001@gmail.com>
2024-11-30 21:08:41 +0530

    implement bloc for authentication

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,31 @@
+// lib/blocs/auth/auth_bloc.dart
+import 'package:bodo_app/blocs/auth/auth_event.dart';
+import 'package:bodo_app/blocs/auth/auth_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bodo_app/repositories/auth_repository.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository _authRepository;
+
+  AuthBloc(this._authRepository) : super(AuthInitial()) {
+    on<RegisterRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        final user = await _authRepository.register(event.email, event.password);
+        emit(AuthSuccess(user: user));
+      } catch (e) {
+        emit(AuthError(e.toString()));
+      }
+    });
+
+    on<LoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        final user = await _authRepository.login(event.email, event.password);
+        emit(AuthSuccess(user: user));
+      } catch (e) {
+        emit(AuthError(e.toString()));
+      }
+    });
+  }
+}
---
+++ lib/blocs/auth/auth_state.dart
@@ -0,0 +1,18 @@
+// lib/blocs/auth/auth_state.dart
+import 'package:bodo_app/models/user_model.dart';
+
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthSuccess extends AuthState {
+  final UserModel user;
+  AuthSuccess({required this.user});
+}
+
+class AuthError extends AuthState {
+  final String error;
+  AuthError(this.error);
+}
